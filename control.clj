87c3e29a8ac82a72c2ee384ca45a44dadc7f65c2
rongtongjin <794220751@qq.com>
2019-08-28 20:15:49 +0800

    add control.clj

---
+++ control.clj
@@ -0,0 +1,31 @@
+(defcluster :rocketmq
+            :clients [{:host "172.16.2.121" :user "root"}
+                      {:host "172.16.2.122" :user "root"}
+                      {:host "172.16.2.123" :user "root"}
+                      {:host "172.16.2.124" :user "root"}
+                      {:host "172.16.2.127" :user "root"}])
+
+(deftask :date "echo date on cluster"  []
+         (ssh "date"))
+
+(deftask :build []
+         (local (run "git submodule init;git submodule update"))
+         (local
+           (run
+             (cd "rocketmq"
+                 (run "mvn -Prelease-all -DskipTests clean install -U")
+                 )))
+
+         (local (run "rm rocketmq-jepsen.tar.gz;tar zcvf rocketmq-jepsen.tar.gz distribution/target/rocketmq-4.6.0-SNAPSHOT.tar.gz dledger-broker.conf")))
+
+
+(deftask :deploy []
+         (scp "rocketmq-jepsen.tar.gz" "/root/")
+         (ssh
+           (run
+             (cd "/root"
+                 (run "rm -rf rocketmq-jepsen/;mkdir rocketmq-jepsen")
+                 (run "tar zxvf rocketmq-jepsen.tar.gz -C rocketmq-jepsen"))
+             (cd "/root/rocketmq-jepsen"
+                 (run "tar zxvf distribution/target/rocketmq-4.6.0-SNAPSHOT.tar.gz"))
+             )))
--- src/rocketmq_jepsen_test/core.clj
+++ src/rocketmq_jepsen_test/core.clj
@@ -1,15 +1,44 @@
 (ns rocketmq-jepsen-test.core
-  (:require [jepsen.cli :as cli]
-            [jepsen.tests :as tests]))
+  (:require [clojure.tools.logging :refer :all]
+            [clojure.string :as cstr]
+            [jepsen [cli :as cli]
+             [control :as c]
+             [db :as db]
+             [tests :as tests]
+             [checker :as checker]
+             [client :as client]
+             [generator :as gen]
+             [nemesis :as nemesis]]
+            [jepsen.checker.timeline :as timeline]
+            [jepsen.control.util :as cu]
+            [jepsen.os :as os]))
 
 
+(defn db
+  "RocketMQ db."
+  []
+  (reify db/DB
+    (setup! [_ test node]
+      (start! test node)
+      (Thread/sleep 20000)
+      )
+
+    (teardown! [_ test node]
+      (stop! node)
+      (Thread/sleep 20000)
+      (c/exec :rm
+              :-rf
+              dledger-data-path))))
 
 (defn rocketmq-jepsen-test
   "Given an options map from the command line runner (e.g. :nodes, :ssh,
   :concurrency, ...), constructs a test map."
   [opts]
   (merge tests/noop-test
-         opts))
+         opts
+         {:name          "rocketmq-jepsen-test"
+          :os            os/noop
+          :db            (db)}))
 
 (defn -main
   "Handles command line arguments. Can either run a test, or a web server for
