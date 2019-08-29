4992c792b4c53d2babfb6d77fb50a1a45341093e
rongtongjin <794220751@qq.com>
2019-08-29 17:03:51 +0800

    Make rocketmq deploy work.

--- rocketmqclient/src/main/java/com/jinrongtong5/rocketmqclient/JepsenTestClient.java
+++ rocketmqclient/src/main/java/com/jinrongtong5/rocketmqclient/JepsenTestClient.java
@@ -7,9 +7,18 @@ public class JepsenTestClient {
     DefaultMQProducer producer = new DefaultMQProducer("producer_group");
     DefaultMQPullConsumer consumer = new DefaultMQPullConsumer("consumer_group");
 
-    public void startup() throws Exception{
+    public void init() throws Exception {
+        producer.setNamesrvAddr("172.16.2.120:9876");
+        consumer.setNamesrvAddr("172.16.2.120:9876");
+    }
+
+    public void startup() throws Exception {
         producer.start();
         consumer.start();
+    }
 
+    public void shutdown() throws Exception {
+        producer.shutdown();
+        consumer.shutdown();
     }
 }
