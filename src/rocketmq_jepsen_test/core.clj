682250e477cf7d1ca2519a88a2bf27067655810d
rongtongjin <794220751@qq.com>
2019-09-08 10:42:57 +0800

    Fix bug

--- src/rocketmq_jepsen_test/core.clj
+++ src/rocketmq_jepsen_test/core.clj
@@ -143,7 +143,7 @@
   "Given an options map from the command line runner (e.g. :nodes, :ssh,
   :concurrency, ...), constructs a test map."
   [opts]
-  (merge tests/noop-test
+  (assoc tests/noop-test
          opts
          {:name   "rocketmq-jepsen-test"
           :os     os/noop
