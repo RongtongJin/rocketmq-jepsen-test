78cf8a9700fcf391d5cd4d0d8aa0d43df787ded0
rongtongjin <794220751@qq.com>
2019-09-14 10:59:26 +0800

    Fix bug

--- project.clj
+++ project.clj
@@ -5,6 +5,6 @@
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :main rocketmq-jepsen-test.core
   :dependencies [[org.clojure/clojure "1.10.0"]
-                 [jepsen "0.1.15" :exclusions [org.clojars.achim/multiset]]
-                 [metametadata/multiset "0.1.1"]
+                 [jepsen "0.1.15" :exclusions [knossos]]
+                 [knossos "0.3.5-SNAPSHOT" :exclusions [org.slf4j/slf4j-log4j12]]
                  [com.jinrongtong5/rocketmq-client "1.0-SNAPSHOT"]])
