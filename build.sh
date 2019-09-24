6259ed9404a1bd5f410553fef4f0efb6b6c20745
rongtongjin <794220751@qq.com>
2019-09-24 17:45:18 +0800

    Add build.sh

---
+++ build.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+git submodule init
+git submodule update
+
+cd openmessaging-storage-dledger
+git checkout -b rocketmq_jepsen_test origin/rocketmq_jepsen_test
+git checkout rocketmq_jepsen_test
+mvn clean install -DskipTests
+cd ..
+
+cd rocketmq
+git checkout -b jepsen_test origin/jepsen_test
+git checkout jepsen_test
+mvn -Prelease-all -DskipTests clean install -U
+cd ..
+
+cd rocketmqclient
+mvn clean install -DskipTests
+cd ..
+
+rm -rf rocketmq-jepsen.tar.gz
+tar zcvf rocketmq-jepsen.tar.gz rocketmq/distribution/target/rocketmq-4.6.0-SNAPSHOT.tar.gz dledger-broker.conf brokershutdown.sh brokerstartup.sh stop_dropcaches.sh
